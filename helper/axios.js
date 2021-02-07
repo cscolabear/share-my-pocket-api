40473fc579a2e87a6f82c2c8dd6fe55c6f036d29
Cola <cscolabear@gmail.com>
2021-02-07 17:17:01 +0800

    feat: add dotent, axios

---
+++ helper/axios.js
@@ -0,0 +1,17 @@
+const axios = require('axios');
+require('dotenv').config()
+
+const pocketApiData = {
+  "consumer_key": process.env.POCKET_CONSUMER_KEY,
+}
+
+const pocketApi = axios.create({
+  baseURL: 'https://getpocket.com/v3/',
+  timeout: 1000,
+  headers: {'X-Accept': 'application/json',},
+});
+
+module.exports = {
+  pocketApiData,
+  pocketApi,
+}
